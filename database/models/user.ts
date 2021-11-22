b046450afcf60c1aa538a7c756914dc33f08ad60
Claudia Font <claudiafontmarques@gmail.com>
2021-11-22 21:21:35 +0100

    added user model database

---
+++ database/models/user.ts
@@ -0,0 +1,35 @@
+const { Schema, model, Types } = require("mongoose");
+
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  id: number;
+  listLandmarks: Array<{ id: number }>;
+}
+
+const userSchema: User = new Schema({
+  name: {
+    type: String,
+    require: true,
+    minlength: 1,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 30,
+  },
+  listsLandmarks: {
+    type: [Types.ObjectId],
+    ref: "Landmark",
+  },
+});
+const UserModel = model("User", userSchema, "Users");
+
+module.exports = UserModel;
