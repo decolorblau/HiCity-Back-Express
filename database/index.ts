0e5d1840442e5ab69a6b3057362e7fda22033e2f
Claudia Font <claudiafontmarques@gmail.com>
2021-11-23 09:32:26 +0100

    added db connection

---
+++ database/index.ts
@@ -0,0 +1,34 @@
+const debug = require("debug")("series:database");
+const chalk = require("chalk");
+const mongoose = require("mongoose");
+
+const connectDB = (connectionDB) =>
+  new Promise<void>((resolve, reject) => {
+    mongoose.set("debug", false);
+
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret._id;
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret.__v;
+      },
+    });
+    mongoose.connect(connectionDB, (error) => {
+      if (error) {
+        debug(chalk.red("The database could not be started", error.message));
+        debug(chalk.red(error.message));
+
+        reject();
+        return;
+      }
+      debug(chalk.green("The database is connected"));
+      resolve();
+    });
+    mongoose.connection.on("close", () => {
+      debug(chalk.green("DB disconnected"));
+    });
+  });
+
+exports = { connectDB };
