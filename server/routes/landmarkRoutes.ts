5ecd0692d9a4d96f79cabcd58126acb8636b2fac
Claudia Font <claudiafontmarques@gmail.com>
2021-11-24 14:08:43 +0100

    fix main direction

--- server/routes/landmarkRoutes.ts
+++ server/routes/landmarkRoutes.ts
@@ -4,5 +4,6 @@ import getLandmarks from "../controllers/landmarkController";
 const router = express.Router();
 
 router.get("/", getLandmarks);
+router.post("/new", getLandmarks);
 
 export default router;
