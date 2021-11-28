e1e48c5984ddf64b6e5b7042b80c768e14abdfa6
Claudia Font <claudiafontmarques@gmail.com>
2021-11-28 11:26:08 +0100

    deleted comment from IError

--- interfaces/IError.ts
+++ interfaces/IError.ts
@@ -3,9 +3,3 @@ import { ValidationError } from "express-validation";
 export default interface IError extends ValidationError {
   code: number | string;
 }
-
-/* export interface IError {
-  message?: string;
-  statusCode?: number;
-}
- */
